139bb291f1209b3f3f4c05dea384df7acce3a5f6
AndrewGharios <98057762+AndrewGharios@users.noreply.github.com>
2026-07-07 17:03:43 +0500

    Add files via upload

---
+++ lab1_4/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        double x;
+        double eps;
+        
+   
+        Console.WriteLine("Введите x: ");
+        while (!double.TryParse(Console.ReadLine(), out x))
+        {
+            Console.WriteLine("Введите корректное число!");
+        }
+
+
+        Console.WriteLine("Введите положительное малое число epsilon: ");
+        while (!double.TryParse(Console.ReadLine(), out eps) || eps <= 0)
+        {
+            Console.WriteLine("Введите положительное число!");
+        }
+
+        double sum = 0.0;
+        double term = 1.0;  
+        int n = 0;           
+
+        while (Math.Abs(term) >= eps)
+        {
+            sum += term;          
+            n++;                   
+            term *= -x * x / n;     // вычисляем следующий член по формуле term(n+1) = term(n) * (-x^2)/(n+1)
+        }
+
+        Console.WriteLine($"Сумма ряда с точностью {eps}: {sum}");
+        Console.WriteLine($"Количество учтенных членов: {n}");
+        Console.WriteLine($"Точное значение exp(-x^2): {Math.Exp(-x * x)}");
+        Console.WriteLine($"Разница: {Math.Abs(sum - Math.Exp(-x * x))}");
+    }
+}
---
+++ lab1_7/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Введите количество строк n: ");
+        int n = int.Parse(Console.ReadLine());
+        
+        Console.Write("Введите количество столбцов m: ");
+        int m = int.Parse(Console.ReadLine());
+        
+        double[,] a = new double[n, m];
+        
+        Console.WriteLine("Введите элементы матрицы:");
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < m; j++)
+            {
+                Console.Write($"a[{i},{j}] = ");
+                a[i, j] = double.Parse(Console.ReadLine());
+            }
+        }
+
+        //вивод матр
+        Console.WriteLine();
+        for (int i = 0; i < n; i++)
+        {   
+            for (int j = 0; j < m; j++)
+            {
+                Console.Write(a[i, j] + " ");
+            }
+            Console.WriteLine();
+        }
+
+        
+        double[] minRow = new double[n];
+        int[] minCol = new int[n];
+        
+        for (int i = 0; i < n; i++)
+        {
+            minRow[i] = a[i, 0];
+            minCol[i] = 0;
+            
+            for (int j = 1; j < m; j++)
+            {
+                if (a[i, j] < minRow[i])
+                {
+                    minRow[i] = a[i, j];
+                    minCol[i] = j;
+                }
+            }
+        }
+        
+        double maxMin = minRow[0];
+        int maxRow = 0;
+        
+        for (int i = 1; i < n; i++)
+        {
+            if (minRow[i] > maxMin)
+            {
+                maxMin = minRow[i];
+                maxRow = i;
+            }
+        }
+        
+        int maxCol = minCol[maxRow];
+        
+        Console.WriteLine($"\nИндексы искомого элемента: [{maxRow}, {maxCol}]");
+        Console.WriteLine($"Значение элемента: {a[maxRow, maxCol]}");
+    }
+}
